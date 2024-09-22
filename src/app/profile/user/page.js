dac9a368aa644293a53e5dc1874140281ca5e6ee
Pranav Kanakdande <68493069+pkanakdande@users.noreply.github.com>
2024-09-22 17:05:29 +0530

    user page edited

--- src/app/profile/user/page.js
+++ src/app/profile/user/page.js
@@ -1,48 +1,8 @@
-"use client"
-import UserContext from "@/context/userContext";
-import { getAllTask } from "@/models/services/taskServices";
-import { useState, useEffect, useContext } from "react";
-
-export default function User() {
-    const [allTask, setAllTask] = useState([]);
-  const {user}=useContext(UserContext)
-  
-    async function getAllTaskUser() {
-        try {
-            const loadUserTask = await getAllTask(); // Fetch tasks
-            setAllTask(loadUserTask.reverse()); // Reverse tasks and set state
-        } catch (error) {
-            console.error("Error fetching tasks:", error.message);
-        }
-    }
-
-    useEffect(() => {
-        getAllTaskUser(); // Call the function to fetch tasks on component mount
-    }, []);
-
-    return (
-        <div className="text-white grid grid-cols-12">
-            <div className="col-span-6 col-start-4">
-                <h1 className="text-3xl">User's Tasks</h1>
-                {allTask.map((task) => (
-                    <div key={task.id} className={`shadow-lg mt-2 rounded-md ${task.status === "completed" ? "bg-green-800" : "bg-gray-800"}`}>
-                        <div className="p-5">
-                            <div className="flex justify-between mb-3 items-center">
-                                <h1 className="text-2xl font-semibold">{task.title}</h1>
-                            </div>
-                            <p className="font-normal">{task.content}</p>
-                            <div className="flex justify-between mt-3">
-                                <p className="text-left">
-                                    Status: <span className="font-bold">{task.status}</span>
-                                </p>
-                                <p className="text-right">
-                                    Author: <span className="font-bold">{task.user?.name || "Unknown"}</span> {/* Access user name directly */}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
+export default function User(){
+    return(
+        <div>
+         <h1>User Page</h1>
         </div>
-    );
+        
+    )
 }
